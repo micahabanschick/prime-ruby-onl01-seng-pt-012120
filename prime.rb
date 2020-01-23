bb025675cce0605b6587852379c969f4d46bc535
Micah Banschick <micha.ban@gmail.com>
2020-01-23 21:08:43 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,12 @@
 # Add  code here!
 def prime?(num)
   (2..(num - 1)).each do |n|
-    return false if num % n == 0
+    if num % n == 0
+      return false 
+    else 
+      return true 
   end
-  true
+  #true
 end
 
 
