569b5029fe88ebf88f999ccbd12b8ba22dedaf92
Micah Banschick <micha.ban@gmail.com>
2020-01-23 20:49:29 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,4 +1,8 @@
 # Add  code here!
 def prime?(integer)
-  all_num = -Float::INFINITY..Float::INFINITY
+  all_num = -Float::INFINITY..Float::INFINITY.to_a
+  prime_num = []
+  all_num.filter do |num|
+    prime_num << 
+  end 
 end 
