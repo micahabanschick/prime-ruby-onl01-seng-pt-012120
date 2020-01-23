f93b0631f858b05bf588ceff6c0597cfe82ca322
Micah Banschick <micha.ban@gmail.com>
2020-01-23 21:27:26 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,7 @@
 # Add  code here!
 def prime?(num)
   p_odds = (2..(num - 1)).to_a.filter{|int| int.odd?}
-  n_odds = ((num - 1)..2).to_a.filter{|int| int.odd?}
+  n_odds = (2..(-num - 1)).to_a.filter{|int| int.odd?}
   if p_odds.any?{|n| num % n == 0}
     false 
   elsif n_odds.any?{|n| num % n == 0}
